d63bca4cbfba20f840307b418af961c690ac2820
Reimar Bauer <rb.proj@gmail.com>
2018-01-18 13:55:03 +0100

    fs icon added to widget

--- fslib/fs_filepicker.py
+++ fslib/fs_filepicker.py
@@ -42,6 +42,7 @@ class FilePicker(QtWidgets.QDialog, ui_filepicker.Ui_Dialog):
                  default_filename=None, show_save_action=False):
         super(FilePicker, self).__init__(parent)
         self.setupUi(self)
+        self.setWindowIcon(QIcon(icons('fs_logo.png', origin='fs')))
         self.file_icon = icons('text-x-generic.png')
         self.dir_icon = icons('folder.png')
         self.selected_dir = None
